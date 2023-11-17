c6529e19cf0769a4f4983e603475212e654937b5
Usman Awan <148188498+UsmanDevCraft@users.noreply.github.com>
2023-11-17 13:12:45 -0800

    Delete usman-app directory

--- usman-app/src/module1.mjs
+++
@@ -1,7 +0,0 @@
-var a = "usman";
-let b = "umer";
-const c = "sidra";
-
-export default a;
-export {b};
-export {c};
--- usman-app/src/module2.mjs
+++
@@ -1,5 +0,0 @@
-import ui, {b,c} from './module1.mjs'
-
-console.log(ui);
-console.log(b);
-console.log(c);
